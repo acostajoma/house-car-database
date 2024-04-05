f70b2f59fff6ac34311ce5e93ee8a637934cd75e
Martin <acostajoma9@gmail..com>
2024-04-05 16:51:39 -0600

    Fixing error

--- favorites/properties.sql
+++ favorites/properties.sql
@@ -11,16 +11,16 @@ ALTER TABLE favorite ENABLE ROW LEVEL SECURITY;
 -- Favorites Policies
 CREATE POLICY "Favorites are only viewable by users who add them."
   ON favorite FOR SELECT
-  USING (SELECT( auth.uid() )= user_id);
+  USING ((SELECT auth.uid() )= user_id);
 
 CREATE POLICY "Users can insert their own favorites."
   ON favorite FOR INSERT
-  WITH CHECK (SELECT( auth.uid() ) = user_id);
+  WITH CHECK ((SELECT auth.uid() ) = user_id);
 
 CREATE POLICY "Users can update own favorites."
   ON favorite FOR UPDATE
-  USING (SELECT( auth.uid() ) = user_id);
+  USING ((SELECT auth.uid() ) = user_id);
 
   CREATE POLICY "Favorites are only deletable by users who add them."
   ON favorite FOR DELETE
-  USING (SELECT( auth.uid() ) = user_id);
+  USING ((SELECT auth.uid() ) = user_id);
